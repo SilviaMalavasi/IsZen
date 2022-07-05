2444a84cba9d2a639ec254c54147b51c89956e80
Silvia Malavasi <smalavasi24@gmail.com>
2022-07-05 16:35:11 +0200

    font preload

--- header.php
+++ header.php
@@ -5,6 +5,7 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<link rel="preload" href="<?php echo get_stylesheet_directory_uri(); ?>/fonts/Roboto-Regular.woff2" as="font" type="font/woff2" crossorigin>
 
 	<?php wp_head(); ?>
 </head>
