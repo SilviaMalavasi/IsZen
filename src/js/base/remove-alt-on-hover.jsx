5b60e1aaf3c0026d547a99bc68420cc8cb5e4392
Silvia Malavasi <smalavasi24@gmail.com>
2023-03-11 08:25:35 +0100

    removealtonhover fix

--- src/js/base/remove-alt-on-hover.jsx
+++ src/js/base/remove-alt-on-hover.jsx
@@ -1,12 +1,16 @@
-export function removeAltOnHover() {}
-(function($) {
-$( "img, iframe" ).mouseenter(function() {
-    var title = $(this).attr("title");
-    $(this).attr("tmp_title", title);
-    $(this).attr("title","");
-  });
-  $( "img, iframe" ).mouseleave(function() {
-    var title = $(this).attr("tmp_title");
-    $(this).attr("title",title);
+export function removeAltOnHover() {
+  (function($) {
+    
+        $( "img, iframe" ).mouseenter(function() {
+          var title = $(this).attr("title");
+          $(this).attr("tmp_title", title);
+          $(this).attr("title","");
+        });
+      
+        $( "img, iframe" ).mouseleave(function() {
+          var title = $(this).attr("tmp_title");
+          $(this).attr("title",title);
+        });
+      
   })(jQuery);
-});
+  }
