9abf3aa48cac58a5e9653fec750e0e0c910509b7
Silvia Malavasi <smalavasi24@gmail.com>
2023-05-22 13:07:02 +0200

    semplificazione scrollsmoother

--- src/js/base/scrollsmoother.jsx
+++ src/js/base/scrollsmoother.jsx
@@ -1,17 +1,11 @@
-import $ from "jquery";
+export async function doScrollSmoother() {
+  const { gsap } = await import("gsap");
+  const { ScrollTrigger } = await import("gsap/ScrollTrigger");
+  const { ScrollSmoother } = await import("gsap/ScrollSmoother");
 
-export function doScrollSmoother() {
-  async function loadScrollSmoother() {
-    const { gsap } = await import("gsap");
-    const { ScrollTrigger } = await import("gsap/ScrollTrigger");
-    const { ScrollSmoother } = await import("gsap/ScrollSmoother");
+  gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
-    gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
-
-    ScrollSmoother.create({
-      smooth: 1,
-    });
-  }
-
-  loadScrollSmoother();
+  ScrollSmoother.create({
+    smooth: 1,
+  });
 }
