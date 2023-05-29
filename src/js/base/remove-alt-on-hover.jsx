3586a171671672360b582cdf8d9e3e59d084138c
Silvia Malavasi <smalavasi24@gmail.com>
2023-05-29 10:56:35 +0200

    mousenter remove fix

--- src/js/base/remove-alt-on-hover.jsx
+++ src/js/base/remove-alt-on-hover.jsx
@@ -1,13 +1,13 @@
 import $ from "jquery";
 
 export function removeAltOnHover() {
-  $("img, iframe").mouseenter(function () {
+  $("img, iframe, a").on("mouseenter", function () {
     var title = $(this).attr("title");
     $(this).attr("tmp_title", title);
     $(this).attr("title", "");
   });
 
-  $("img, iframe").mouseleave(function () {
+  $("img, iframe, a").on("mouseleave", function () {
     var title = $(this).attr("tmp_title");
     $(this).attr("title", title);
   });
